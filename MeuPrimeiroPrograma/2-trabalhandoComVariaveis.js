85fc589e5865cad97fa120e5e2226c7c13854e21
laramcj <lara.mcj@gmail.com>
2021-07-28 22:03:44 -0300

    variaveis e atribuições

---
+++ MeuPrimeiroPrograma/2-trabalhandoComVariaveis.js
@@ -0,0 +1,14 @@
+//Console.log("Trabalhando com Variáveis");
+console.log("Trabalhando com Variáveis");
+//JS é case sensitive
+
+idade = 29;
+Idade = 30;
+console.log(idade);
+console.log(Idade);
+//boa prática, usar o mesmo padrão de variáveis, ex: todas as variáveis em minúsculas
+
+const nome = "Ricardo";
+let ano = 2020;
+//Não fazer uma variável sem o tipo de variável:
+//Idade = 30;
---
+++ MeuPrimeiroPrograma/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com Atribuição de Variáveis");
+
+const idade = 29;
+const nome = "Lara";
+const sobrenome = "Maia";
+
+//console.log(nome + sobrenome);
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
