c977f53c4defea78bd5a8cfab75462b3ed3ad9b9
laramcj <lara.mcj@gmail.com>
2021-08-02 18:39:12 -0300

    Ajuste javascript

--- MeuPrimeiroPrograma/8-loops.js
+++ MeuPrimeiroPrograma/8-loops.js
@@ -16,14 +16,17 @@ console.log(listaDeDestinos);
 
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 let contador = 0;
+let destinoExiste = false;
 
 while(contador < 3){
+    
     if(listaDeDestinos[contador]== destino){
-        console.log("Destino existe");
-    }else {
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
     contador +=1;
-}
+};
+
+console.log("Destino existe: ", destinoExiste);
 
 
